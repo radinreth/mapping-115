e5e3e46ad2de4975d19a04a3b847a0663c22d2ae
radinreth <radin.reth@gmail.com>
2020-04-23 14:38:49 +0700

    render markers

--- app/javascript/application/index.js
+++ app/javascript/application/index.js
@@ -1,6 +1,5 @@
 document.addEventListener("turbolinks:load", function(){
   const currentPage = App.helper.getCurrentPage();
-  console.log(currentPage)
   if (App[currentPage]) {
     App[currentPage]().init();
   }
--- app/javascript/welcome/index.js
+++ app/javascript/welcome/index.js
@@ -1,15 +1,26 @@
 App.WelcomeIndex = () => {
+  let map = null
+
   const init = () => {
     renderMap()
+    renderMarkers()
   }
 
   const renderMap = () => {
-    let map = L.map("map", { zoom: 13 })
+    map = L.map("map", { zoom: 13 })
     map.fitBounds([
       [10.4865436874, 102.3480994],
       [14.5705838078, 107.614547968]
     ])
     map.addLayer(osm())
+  }
+
+  const renderMarkers = () => {
+    console.log(gon.locations)
+    gon.locations.forEach((loc) => {
+      let marker = L.marker([loc.lat, loc.lng])
+      marker.addTo(map)
+    })
   }
 
   const osm = () => {
