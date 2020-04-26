c8d233a7b3eb45fe1a57b610474b38e11da20019
radinreth <radin.reth@gmail.com>
2020-04-26 15:42:28 +0700

    bind popup, dynamic redius

--- app/javascript/welcome/index.js
+++ app/javascript/welcome/index.js
@@ -1,5 +1,9 @@
 App.WelcomeIndex = () => {
   let map = null
+  let defaultRadiusSize = 5
+  let maxRadiusSize = 18
+  let SW = [10.4865436874, 102.3480994]
+  let NE = [14.5705838078, 107.614547968]
 
   const init = () => {
     renderMap()
@@ -8,23 +12,25 @@ App.WelcomeIndex = () => {
 
   const renderMap = () => {
     map = L.map("map", { zoom: 13 })
-    map.fitBounds([
-      [10.4865436874, 102.3480994],
-      [14.5705838078, 107.614547968]
-    ])
+    map.fitBounds([SW, NE])
     map.addLayer(osm())
   }
 
   const renderMarkers = () => {
     gon.locations.forEach((loc) => {
+      callersCount = loc.nested_count
+      increasedSize = defaultRadiusSize + callersCount
+
       let marker = L.circleMarker([loc.lat, loc.lng], {
         color: "red",
         fillColor: "#fc7a4e",
         fillOpacity: 0.8,
         weight: 1,
         opacity: 1,
-        radius: 5
+        radius: (increasedSize > maxRadiusSize ? maxRadiusSize : increasedSize)
       }).addTo(map)
+
+      marker.bindPopup(`Callers: ${callersCount}`)
     })
   }
 
