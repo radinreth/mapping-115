d563522ce59a9fab19d8d30b7e337fe54f1a2be3
radinreth <radin.reth@gmail.com>
2020-04-26 15:41:49 +0700

    normalize params

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def normalize_params(hash, result = {})
+    hash.keys.each do |key|
+      if hash[key].is_a? ActionController::Parameters
+        result = normalize_params(hash[key], result)
+      else
+        result[mapper[key]] = hash[key]
+      end
+    end
+    result
+  end
+
+  def mapper
+    @mapper ||= { phone_number: 'phone_number', Latitude: 'lat', Longitude: 'lng', NCDD_Code: 'ncdd_code' }
+    @mapper.with_indifferent_access
+  end
 end
