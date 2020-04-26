7d219b225ea7e78828e4ffcb5915ddaa42c2ad19
radinreth <radin.reth@gmail.com>
2020-04-26 15:41:27 +0700

    include callers_count

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    gon.locations = Location.where(kind: 'province')
+    gon.locations = Location.where(kind: 'province').as_json(methods: :nested_count)
   end
 end
