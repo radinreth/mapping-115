fa266e5ba1a8ffe8909270f1f1bb5ee9fe5e43e8
radinreth <radin.reth@gmail.com>
2020-04-27 11:30:40 +0700

    update map tile

--- app/javascript/welcome/index.js
+++ app/javascript/welcome/index.js
@@ -18,24 +18,25 @@ App.WelcomeIndex = () => {
 
   const renderMarkers = () => {
     gon.locations.forEach((loc) => {
-      callersCount = loc.nested_count
+      callersCount = loc.callers_count
       increasedSize = defaultRadiusSize + callersCount
 
       let marker = L.circleMarker([loc.lat, loc.lng], {
-        color: "red",
-        fillColor: "#fc7a4e",
+        color: (callersCount == 0 ?"#004e98" : "red"),
+        fillColor: (callersCount == 0 ?"#3a6ea5" : "#d62828"),
         fillOpacity: 0.8,
         weight: 1,
         opacity: 1,
         radius: (increasedSize > maxRadiusSize ? maxRadiusSize : increasedSize)
       }).addTo(map)
 
-      marker.bindPopup(`Callers: ${callersCount}`)
+      marker.bindPopup(`ចំនួនតេចូល:${callersCount}`)
     })
   }
 
   const osm = () => {
-    let osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+    // let osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+    let osmUrl = 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png'
     let osmAttrib = "Map data © <a href='https://openstreetmap.org'>OpenStreetMap</a> contributors"
     let osm = new L.TileLayer(osmUrl, { minZoom: 5, maxZoom: 11, attribution: osmAttrib });
     return osm
