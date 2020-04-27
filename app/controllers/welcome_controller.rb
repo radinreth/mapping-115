d82f16f113d21b5dbbc1bf0eb9ddf58ab591c654
radinreth <radin.reth@gmail.com>
2020-04-27 11:33:14 +0700

    only callers exist

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,8 @@
 class WelcomeController < ApplicationController
   def index
     gon.locations = Location.where(kind: (params[:kind] || 'province'))
-                            .where.not(lat: nil, lng: nil)
-                            .as_json(methods: :nested_count)
+                            .where('callers_count > 0')
+                            .where.not(lat: nil, lng: nil).as_json
+                            # .as_json(methods: :nested_count)
   end
 end
