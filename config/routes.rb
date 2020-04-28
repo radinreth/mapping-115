a3af89cc9a1586992d68f98227ccb7ed4c9c3b91
radinreth <radin.reth@gmail.com>
2020-04-28 20:36:58 +0700

    add external service url

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   root 'welcome#index'
 
+  mount Sidekiq::Web => '/sidekiq'
+  resource :manifest, only: [:show]
+  resources :caller_logs, only: [:create]
   namespace :api, format: :json do
     resources :callers, only: [:create]
   end
