b2c58ffe563296fc8f2a51ce9aad58e43cb1aeff
radinreth <radin.reth@gmail.com>
2020-04-28 22:36:26 +0700

    rescue caller service

--- app/services/caller_service.rb
+++ app/services/caller_service.rb
@@ -2,11 +2,15 @@ require 'rest-client'
 
 class CallerService
   def self.run(phone_number)
-    # get payload from external service
-    uri = "#{ENV['TELCO_URL']}/#{phone_number}?token=#{ENV['TOKEN']}"
-    response = RestClient.get uri
+    begin
+      # get payload from external service
+      uri = "#{ENV['TELCO_URL']}/#{phone_number}?token=#{ENV['TOKEN']}"
+      response = RestClient.get uri
 
-    # save end-result to mapping-115
-    RestClient.post api_callers_path, response
+      # save end-result to mapping-115
+      RestClient.post api_callers_path, response
+    rescue StandardError => e
+      Rails.logger.debug "Request failed: #{e.message}"
+    end
   end
 end
